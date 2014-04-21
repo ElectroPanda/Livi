59991ad76d0a32e6ff5321314668fccfb752b3a0
ElectroPanda <nus-ii@mail.ru>
2014-04-21 07:33:30 +0400

    Start wordasisst class
    ParamRamRam

---
+++ Sourse/Livi/WordAsisst.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Сделано в SharpDevelop.
+ * Пользователь: Илья
+ * Дата: 21.04.2014
+ * Время: 7:27
+ * 
+ * Для изменения этого шаблона используйте Сервис | Настройка | Кодирование | Правка стандартных заголовков.
+ */
+using System;
+
+namespace Livi
+{
+	/// <summary>
+	/// Description of WordAsisst.
+	/// </summary>
+	public class WordAsisst
+	{
+		public WordAsisst()
+		{
+		} //end constructor
+		
+		/*public static Boolean HaveCapetalLetter (string a)
+		{
+			private Boolean b;
+			b=false;
+			return b;
+		} // End HaveCapetalLetter method
+		*/
+		
+	} // end WordAsiss class 
+} //end name space Livi
